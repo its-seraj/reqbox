bee4335789720e8080230af76ae5501542f2b96f
seraj-b2 <125463271+seraj-b2@users.noreply.github.com>
2024-12-02 21:19:04 +0530

    ui changes

--- src/components/leftPanel/curlItem.jsx
+++ src/components/leftPanel/curlItem.jsx
@@ -3,19 +3,34 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 
-const Method = ({ type }) => (
-  <>
-    {type === "GET" ? (
-      <div style={{ fontSize: "0.9em", fontWeight: "600", color: "green" }}>{type}</div>
-    ) : type === "POST" ? (
-      <div style={{ fontSize: "0.9em", fontWeight: "600", color: "orange" }}>{type}</div>
-    ) : (
-      <></>
-    )}
-  </>
-);
-export const CurlItem = ({ currRequest, currFolderId, requestClickHandler }) => {
+const Method = ({ type }) => {
+  const getTypeStyle = (type) => {
+    switch (type) {
+      case "GET":
+        return { fontSize: "0.9em", fontWeight: "600", color: "green" };
+      case "POST":
+        return { fontSize: "0.9em", fontWeight: "600", color: "orange" };
+      case "PUT":
+        return { fontSize: "0.9em", fontWeight: "600", color: "blue" };
+      case "PATCH":
+        return { fontSize: "0.9em", fontWeight: "600", color: "purple" };
+      case "DELETE":
+        return { fontSize: "0.9em", fontWeight: "600", color: "red" };
+      case "HEAD":
+        return { fontSize: "0.9em", fontWeight: "600", color: "brown" };
+      case "OPTIONS":
+        return { fontSize: "0.9em", fontWeight: "600", color: "gray" };
+      default:
+        return null;
+    }
+  };
 
+  const style = getTypeStyle(type);
+
+  return style ? <div style={style}>{type}</div> : null;
+};
+
+export const CurlItem = ({ currRequest, currFolderId, requestClickHandler }) => {
   return (
     <>
       <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }} component="nav" aria-labelledby="nested-list-subheader">
